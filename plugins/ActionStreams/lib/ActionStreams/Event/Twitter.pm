4776e7c6c6c59648007d96b20c2fc828b241ff9f
Mark Paschal <markpasc@markpasc.org>
2008-09-22 18:32:42 +0000

    Move autolink building into subs that can be overridden Use updated hashtag regexp

--- plugins/ActionStreams/lib/ActionStreams/Event/Twitter.pm
+++ plugins/ActionStreams/lib/ActionStreams/Event/Twitter.pm
@@ -3,6 +3,21 @@ package ActionStreams::Event::Twitter;
 
 use strict;
 
+use MT::Util qw( encode_url );
+
+sub profile_link_for_name {
+    my $self = shift;
+    my ($name) = @_;
+    return qq{<a href="http://twitter.com/$name">$name</a>};
+}
+
+sub search_link_for_tag {
+    my $self = shift;
+    my ($tag) = @_;
+    my $enc_tag = encode_url($tag);
+    return qq{<a href="http://search.twitter.com/search?tag=$enc_tag">$tag</a>};
+}
+
 sub autolink {
     my $self = shift;
     my ($text) = @_;
@@ -10,23 +25,30 @@ sub autolink {
 
     # autolink URLs
     $text =~ s{
-        ( ^ | [\s.:;?\-\]<\(] )
+        ( \A | [\s.:;?\-\]<\(] )
         (
             https?://
             [-\w;/?:@&=+\$\.!~*'()%,#]+
             [\w/]
         )
-        (?= $ | [\s\.,!:;?\-\[\]>\)] )
-    }{$1<a href="$2">$2</a>}gmx;
+        (?= \z | [\s\.,!:;?\-\[\]>\)] )
+    }{$1<a href="$2">$2</a>}xmsg;
 
     # twitter ids (@bradchoate)
-    $text =~ s{(\s|^)@([A-Za-z0-9_]+)}{$1@<a href="http://www.twitter.com/$2">$2</a>}gs;
+    $text =~ s{
+        (?: (?<= \A @ )    # leading @
+          | (?<= \s @ ) )  # or space and @
+        (\w+)              # and a name
+    }{ $self->profile_link_for_name($1) }xmsge;
 
-    # hash tags (#perl) (linking to summize requests)
-    $text =~ s{(\s|^)#([A-Za-z0-9_]+)}{$1#<a href="http://summize.com/search?tag=$2">$2</a>}gs;  
+    # hash tags (#perl)
+    $text =~ s{
+        (?: (?<= \A\# ) | (?<= \s\# ) )  # BOT or whitespace
+        (\w\S*\w)      # tag
+        (?<! 's )      # but don't end with 's
+    }{ $self->search_link_for_tag($1) }xmsge;
 
     return $text;
 }
 
 1;
-
