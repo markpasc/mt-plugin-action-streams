1deb6700b11105a6c5e77e84aa04160039c929c7
Mark Paschal <markpasc@markpasc.org>
2009-05-27 22:54:19 +0000

    Add localizations from Motion AS (compare revision 1536)

---
+++ plugins/ActionStreams/lib/ActionStreams/L10N/en_us.pm
@@ -0,0 +1,14 @@
+# Movable Type (r) (C) 2001-2008 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id: $
+
+package ActionStreams::L10N::en_us;
+
+use strict;
+use base 'ActionStreams::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
